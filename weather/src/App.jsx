98802a6a4f315316a61f918143c59b6b25fb25c0
MOURI <srimouriya2004@gmail.com>
2024-07-22 11:00:29 +0530

    first commit

---
+++ weather/src/App.jsx
@@ -0,0 +1,12 @@
+import  'react'
+import Weather from'./components/weatherapp'
+const App = () => {
+  return (
+    <div className='app'>
+      <Weather/>
+    </div>
+    
+  )
+}
+
+export default App
